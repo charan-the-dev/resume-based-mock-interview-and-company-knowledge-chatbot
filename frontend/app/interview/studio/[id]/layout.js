46d044d68cd768dc325fe8e560104551b0258bbb
charan-the-dev <mailto.charany@gmail.com>
2025-11-11 08:03:20 +0530

    update entire interview simulation codebase with new changes

--- frontend/app/interview/studio/[id]/layout.js
+++ frontend/app/interview/studio/[id]/layout.js
@@ -1,4 +1,3 @@
-import ChatLayout from '@/components/ChatLayout';
 import { doesInterviewExist } from '@/lib/actions/interview.action'
 
 export default async function layout({ children, params }) {
@@ -12,8 +11,7 @@ export default async function layout({ children, params }) {
 
     return (
         <>
-            {/* {children} */}
-            <ChatLayout interviewId={id} />
+            {children}
         </>
     )
 }
